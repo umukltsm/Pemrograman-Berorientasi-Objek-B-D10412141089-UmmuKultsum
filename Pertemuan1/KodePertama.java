84b307b4491b26ee7c3b9db5cb2d13cba7548a07
umukltsm <d1041241089@student.untan.ac.id>
2026-02-05 15:19:41 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,12 @@
+public class KodePertama {
+public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah Program Java pertama saya.");
+    }
+}
+
+/*
+Nama: Ummu Kultsum
+NIM: D1041241089
+Tanggal: 05/02/2026
+*/
