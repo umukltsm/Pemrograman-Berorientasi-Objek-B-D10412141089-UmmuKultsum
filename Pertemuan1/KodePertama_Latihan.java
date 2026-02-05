cf8e1a6ed56ba3111c1953e5531289852a7245c9
umukltsm <d1041241089@student.untan.ac.id>
2026-02-05 15:22:02 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama_Latihan.java
@@ -0,0 +1,15 @@
+public class KodePertama_Latihan {
+    public static void main(String[] args) {
+        System.out.println("Nama        : Ummu Kultsum");
+        System.out.println("Nim         : D1041241089");
+        System.out.println("Semester    : 4");
+        System.out.println("Prodi       : Informatika");
+    }
+}
+
+/*
+Nama: Ummu Kultsum
+NIM: D1041241089
+Tanggal: 05/02/2026
+*/
+
