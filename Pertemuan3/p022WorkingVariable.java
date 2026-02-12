a790a2be0500e17a6f59c87707e61e13333afe9e
umukltsm <d1041241089@student.untan.ac.id>
2026-02-12 14:07:16 +0700

    Pertemuan3

---
+++ Pertemuan3/p022WorkingVariable.java
@@ -0,0 +1,13 @@
+public class p022WorkingVariable {
+public static void main(String[] args) {
+        int angka = 10;
+        System.out.println("Nilai awal angka: " + angka);
+        angka = 25;
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041241089
+Tanggal: 12/02/2026
+*/
