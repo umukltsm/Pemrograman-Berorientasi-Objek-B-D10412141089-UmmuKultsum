93d0a2714b9bdfe6bb85e31b27ce8332db01edb1
umukltsm <d1041241089@student.untan.ac.id>
2026-02-12 14:09:24 +0700

    Pertemuan3

---
+++ Pertemuan3/p023PrimitiveData.java
@@ -0,0 +1,22 @@
+public class p023PrimitiveData {
+public static void main(String[] args) {
+        int umur = 20;
+        long populasi = 8000000000L;
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+        char nilaiHuruf = 'A';
+        boolean lulus = true;
+
+        System.out.println("Umur        : " + umur);
+        System.out.println("Populasi    : " + populasi);
+        System.out.println("Tinggi Badan: " + tinggi);
+        System.out.println("IPK         : " + ipk);
+        System.out.println("Nilai Huruf : " + nilaiHuruf);
+        System.out.println("Status Lulus: " + lulus);
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041241089
+Tanggal: 12/02/2026
+*/
