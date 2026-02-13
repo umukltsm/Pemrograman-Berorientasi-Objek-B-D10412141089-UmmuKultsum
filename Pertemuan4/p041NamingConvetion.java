c26d913cd689f783f834a2060f74441114003b2a
umukltsm <d1041241089@student.untan.ac.id>
2026-02-13 08:55:56 +0700

    Pertemuan4

---
+++ Pertemuan4/p041NamingConvetion.java
@@ -0,0 +1,18 @@
+public class p041NamingConvetion {
+    public static final double PI = 3.14;
+    public static void main(String[] args) {
+    // Variabel dengan camelCase
+    int studentAge = 20;
+    String studentName = "Budi";
+    boolean isActive = true;
+    System.out.println("Nama        : " + studentName);
+    System.out.println("Umur        : " + studentAge);
+    System.out.println("Status      : " + isActive);
+    System.out.println("Konstanta PI: " + PI);
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041241089
+Tanggal: 13/02/2026
+*/
