b567779e4c79bf7598db165f3533509bb0a7d8bb
umukltsm <d1041241089@student.untan.ac.id>
2026-02-13 08:02:53 +0700

    Pertemuan3

---
+++ Pertemuan3/LatihDataMahasiswa.java
@@ -0,0 +1,23 @@
+public class LatihDataMahasiswa {
+    public static void main(String[] args) {
+        long nim = 2301010001L;
+        String nama = "Budi Santoso";
+        char kelas = 'A';
+        boolean statusAktif = true;
+        double ipk = 3.75;
+        int totalSks = 72;
+
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM         : " + nim);
+        System.out.println("Nama        : " + nama);
+        System.out.println("Kelas       : " + kelas);
+        System.out.println("Status Aktif: " + statusAktif);
+        System.out.println("IPK Semester: " + ipk);
+        System.out.println("Total SKS   : " + totalSks);
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041241089
+Tanggal: 13/02/2025
+*/
