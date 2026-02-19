61f96125d36a537ae5c5b7d2e0588b57f82c1484
umukltsm <d1041241089@student.untan.ac.id>
2026-02-19 10:48:54 +0700

    Pertemuan4
    Latihan 3 Tugas Mandiri

---
+++ Pertemuan4/LatihanCounter.java
@@ -0,0 +1,29 @@
+public class LatihanCounter {
+    public static void main(String[] args) {
+        int nilaiAwal = 10;
+
+        System.out.println("=== COUNTER ===");
+        System.out.println("Nilai awal: " + nilaiAwal); 
+
+        nilaiAwal++;
+        nilaiAwal++; 
+        System.out.println("Setelah ++ (2x): " + nilaiAwal);
+
+        nilaiAwal--;
+        System.out.println("Setelah --: " + nilaiAwal);
+
+        nilaiAwal+=5;
+        System.out.println("Setelah += 5: " + nilaiAwal);
+
+        nilaiAwal-=3;
+        System.out.println("Setelah -= 3: " + nilaiAwal);
+
+        nilaiAwal*=2;
+        System.out.println("Setelah *= 2: " + nilaiAwal);
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041241089
+Tanggal: 19/02/2026
+*/
