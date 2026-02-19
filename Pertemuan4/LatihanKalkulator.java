83cb4bd6582426cd1a2583a961fd30ae0c4e6899
umukltsm <d1041241089@student.untan.ac.id>
2026-02-19 10:42:36 +0700

    Pertemuan4
    Latihan 2 Tugas Mandiri

---
+++ Pertemuan4/LatihanKalkulator.java
@@ -0,0 +1,21 @@
+public class LatihanKalkulator {
+    public static void main(String[] args) {
+    int angka1 = 25;
+    int angka2 = 4;
+    
+    System.out.println("=== KALKULATOR ===");
+    System.out.println("Angka 1: " + angka1);
+    System.out.println("Angka 2: " + angka2);
+    System.out.println("---");
+    System.out.println("Penjumlahan: " + (angka1 + angka2));
+    System.out.println("Pengurangan: " + (angka1 - angka2));
+    System.out.println("Perkalian: " + (angka1 * angka2));
+    System.out.println("Pembagian: " + (angka1 / angka2));
+    System.out.println("Sisa Bagi: " + (angka1 % angka2));   
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041241089
+Tanggal: 19/02/2026
+*/
