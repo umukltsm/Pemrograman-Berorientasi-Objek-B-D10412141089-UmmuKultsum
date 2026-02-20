7e42df8be552adfe7a36d26f0074a374444692b1
umukltsm <d1041241089@student.untan.ac.id>
2026-02-19 21:14:41 -0500

    Pertemuan9

---
+++ Pertemuan9/ObjekDanClass.java
@@ -0,0 +1,29 @@
+package Pertemuan9;
+class Mobil{
+    String merk;
+    String warna;
+    int tahun;
+    int kecepatan;
+
+    public void percepat(){
+        System.out.println("Kecepatan: " + kecepatan);
+    }
+    public void tampilkanInfo(){
+        System.out.println("Merk     : " + merk);
+        System.out.println("Warna    : " + warna);
+        System.out.println("Tahun    : " + tahun);
+        System.out.println("Kecepatan: " + kecepatan + " km/jam");
+    }
+}
+public class ObjekDanClass {
+    public static void main(String[] args) {
+        Mobil obj = new Mobil();
+        obj.merk = "BMW";
+        obj.warna = "Hitam";
+        obj.tahun = 2025;
+        obj.kecepatan = 1000;
+        
+        System.out.println("Informasi Mobil");
+        obj.tampilkanInfo();
+    }   
+}
