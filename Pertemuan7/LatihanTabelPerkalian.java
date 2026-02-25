512ef30749b67e52fa6e578d038b451ea6a767c8
umukltsm <d1041241089@student.untan.ac.id>
2026-02-25 21:11:18 +0700

    Pertemuan7
    Latihan 3 Pertemuan 7

---
+++ Pertemuan7/LatihanTabelPerkalian.java
@@ -0,0 +1,17 @@
+package Pertemuan7;
+
+public class LatihanTabelPerkalian {
+    public static void main(String[] args) {
+        System.out.println("=== TABEL PERKALIAN 7 ===");
+        int angka = 7;
+        for (int i=1; i<=12; i++){
+            int hasil = angka * i; 
+            System.out.println(angka + " x " + i + " = " + hasil);
+        }
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041214089
+Tanggal: 25/02/2026
+*/
