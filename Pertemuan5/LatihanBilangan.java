81f997dee431d61bddefa714e1c5e8870b138033
umukltsm <d1041241089@student.untan.ac.id>
2026-02-25 20:22:45 +0700

    Pertemuan5
    Latihan 1 Pertemuan 5

---
+++ Pertemuan5/LatihanBilangan.java
@@ -0,0 +1,18 @@
+package Pertemuan5;
+
+public class LatihanBilangan {
+    public static void main(String[] args) {
+        int number = 12;
+        System.out.println("=== PEMERIKSAAN BILANGAN ===");
+        System.out.println("Bilangan: " + number);
+        if(number > 0) {
+            System.out.println("Jenis: Bilangan positif");
+        } else if (number < 0){
+            System.out.println("Jenis: Bilangan negatif");
+        } else {
+            System.out.println("Jenis: Bilangan nol");
+        }
+        String parity = (number % 2 == 0) ? "Genap" : "Ganjil";
+        System.out.println("Tipe: " + parity);
+    }
+}
