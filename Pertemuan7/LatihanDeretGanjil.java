d937dd199e1ac7c00eaac501e9a4aee2ca6962f9
umukltsm <d1041241089@student.untan.ac.id>
2026-02-25 20:59:49 +0700

    Pertemuan7
    Latihan 1 Pertemuan 7

---
+++ Pertemuan7/LatihanDeretGanjil.java
@@ -0,0 +1,20 @@
+package Pertemuan7;
+
+public class LatihanDeretGanjil {
+    public static void main(String[] args) {
+        int angka = 1;
+        System.out.println("=== BILANGAN GANJIL 1-15 ===");
+
+        while (angka <= 15){
+            if (angka % 2 !=0){
+                System.out.print(angka + " ");
+            }
+            angka++;
+        }
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041214089
+Tanggal: 25/02/2026
+*/
