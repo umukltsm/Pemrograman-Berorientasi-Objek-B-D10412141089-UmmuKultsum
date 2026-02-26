292b19f1951ad45f213805eecec4d9382d08ad28
umukltsm <d1041241089@student.untan.ac.id>
2026-02-26 21:20:37 +0700

    Pertemuan8
    Latihan 3 Pertemuan 8

---
+++ Pertemuan8/LatihanTotalGenap.java
@@ -0,0 +1,23 @@
+package Pertemuan8;
+
+public class LatihanTotalGenap {
+    public static void main(String[] args) {
+        int total = 0;
+        System.out.println("=== TOTAL BILANGAN GENAP 1-20 ===");
+
+        for (int i = 1; i <= 20; i++){
+            if (i % 2!= 0){
+                continue;
+            }
+            total += i;
+            System.out.println("Tambah: " + i + " -> Total: " + total);
+        }
+        System.out.println("---");
+        System.out.println("Total akhir: " + total);
+    }
+}
+/*
+Nama: Ummu Kultsum
+NIM: D1041214089
+Tanggal: 25/02/2026
+*/
